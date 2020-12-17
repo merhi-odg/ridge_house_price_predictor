3e891248f05aa481b113c79b3d71a7cdcb7e5d9e
merhi-odg <sami.merhi@opendatagroup.com>
2020-12-16 19:21:40 -0600

    add output schema

--- ridge_house_price_recommender.py
+++ ridge_house_price_recommender.py
@@ -1,38 +1,46 @@
 # fastscore.schema.0: input_schema.avsc
-# fastscore.slot.1: in-use
+# fastscore.schema.1: output_schema.avsc
+
 
 import pandas as pd
 import pickle
 import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import warnings
+
 
 # modelop.init
 def begin():
+
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    warnings.simplefilter(action='ignore', category=UserWarning)
+
     global ridge_model, train_encoded_columns
     ridge_model = pickle.load(open("ridge_model.pickle", "rb"))
     train_encoded_columns = pickle.load(open("train_encoded_columns.pickle", "rb"))
 
+    print("\ntype(ridge_model): ", type(ridge_model), flush=True)
+    print("\ntype(train_encoded_columns): ", type(train_encoded_columns), flush=True)
+
+    
+
 # modelop.score
 def action(data):
     
-    print(type(data))
-    data = pd.DataFrame([data])
-    
-    if 'SalePrice' in data.columns:  # Checking to see if data is labeled
-        labeled=True
-        actuals = data['SalePrice']  # Saving actuals (Sale prices)
-        data.drop('SalePrice', axis=1, inplace=True)
-    else:
-        labeled=False
-        
-    print("Labeled: ", labeled)
+    print("\ntype(data): ", type(data), flush=True)
 
-    data_ID = data['Id']  # Saving the Id column
+    data = pd.DataFrame(data)
+
+    data_ID = data['Id']  # Saving the Id column then dropping it
     data.drop("Id", axis=1, inplace=True)
 
+    # Imputations
     data['MasVnrType'] = data['MasVnrType'].fillna('None')
     data['MasVnrArea'] = data['MasVnrArea'].fillna(0)
     data['Electrical'] = data['Electrical'].fillna('SBrkr')
-    data['LotFrontage'] = data.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+    data['LotFrontage'] = data.groupby('Neighborhood')['LotFrontage'].transform(
+        lambda x: x.fillna(x.median())
+    )
     data['GarageYrBlt'] = data['GarageYrBlt'].fillna(data['YearBuilt'])
 
     data['MSZoning'] = data['MSZoning'].fillna('RL')
@@ -51,23 +59,27 @@ def action(data):
     data['Exterior2nd'] = data['Exterior2nd'].fillna('VinylSd')
     data['Exterior1st'] = data['Exterior1st'].fillna('VinylSd')
 
+    # Transform some features into catgeoricals
     data['MSSubClass']  = pd.Categorical(data.MSSubClass)
     data['YrSold']  = pd.Categorical(data.YrSold)
     data['MoSold']  = pd.Categorical(data.MoSold)
     
-    print("shape: ", data.shape)
+    print("\nshape: ", data.shape, flush=True)
 
     #  Computing total square-footage as a new feature
     data['TotalSF'] = data['TotalBsmtSF'] + data['firstFlrSF'] + data['secondFlrSF']
 
     #  Computing total 'porch' square-footage as a new feature
-    data['Total_porch_sf'] = (data['OpenPorchSF'] + data['threeSsnPorch'] + data['EnclosedPorch'] 
-                             + data['ScreenPorch'] + data['WoodDeckSF'])
+    data['Total_porch_sf'] = (
+        data['OpenPorchSF'] + data['threeSsnPorch'] + data['EnclosedPorch'] 
+        + data['ScreenPorch'] + data['WoodDeckSF']
+    )
 
     #  Computing total bathrooms as a new feature
-    data['Total_Bathrooms'] = (data['FullBath'] + (0.5 * data['HalfBath']) +
-                                data['BsmtFullBath'] + (0.5 * data['BsmtHalfBath']))
-
+    data['Total_Bathrooms'] = (
+        data['FullBath'] + (0.5 * data['HalfBath']) + data['BsmtFullBath'] 
+        + (0.5 * data['BsmtHalfBath'])
+    )
 
     # Engineering some features into Booleans
     f = lambda x: bool(1) if x > 0 else bool(0)
@@ -77,59 +89,81 @@ def action(data):
     data['has_bsmt'] = data['TotalBsmtSF'].apply(f)
     data['has_fireplace'] = data['Fireplaces'].apply(f)
 
+    # Drop some unnecassary features
     data = data.drop(['threeSsnPorch', 'PoolArea', 'LowQualFinSF'], axis=1)
     
-    print("shape before get dummies: ", data.shape)
-    cat_cols = ['MSSubClass', 'MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 
-                'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 
-                'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 
-                'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 
-                'HeatingQC', 'CentralAir', 'Electrical', 'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 
-                'GarageFinish', 'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 
-                'MoSold', 'YrSold', 'SaleType','SaleCondition', 'has_pool', 'has_garage', 'has_bsmt', 'has_fireplace']
+    print("\nshape before get dummies: ", data.shape, flush=True)
+
+    cat_cols = [
+        'MSSubClass', 'MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 
+        'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 
+        'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 
+        'Exterior1st', 'Exterior2nd', 'MasVnrType', 'ExterQual', 'ExterCond', 
+        'Foundation', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+        'BsmtFinType2', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 
+        'KitchenQual', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 
+        'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 
+        'MiscFeature', 'MoSold', 'YrSold', 'SaleType', 'SaleCondition', 'has_pool', 
+        'has_garage', 'has_bsmt', 'has_fireplace'
+    ]
 
     encoded_features = pd.get_dummies(data, columns = cat_cols)
     
-    print("shape after get dummies: ", encoded_features.shape)
+    print("\nshape after get dummies: ", encoded_features.shape)
 
     # Matching dummy variables from training set to current dummy variables
     missing_cols = set(train_encoded_columns) - set(encoded_features.columns)
 
-    print("len of Missing Cols: ", len(missing_cols))
+    print("\nlen of Missing Cols: ", len(missing_cols), flush=True)
     for c in missing_cols:
         encoded_features[c] = 0
 
     # Matching order of variables to those used in training
     encoded_features = encoded_features[train_encoded_columns]
     
-    print("shape Encoded Features: ", encoded_features.shape)
+    print("\nshape of encoded_features: ", encoded_features.shape)
 
-    models = {'Ridge': ridge_model}
+    # Computing predictions for each record
+    log_predictions = ridge_model.predict(encoded_features)
 
-    log_predictions = {}  # Model was trained on log(SalePrice)
-    RMSEs = {}  # Root mean square error
-
-    for name, model in models.items():
-        log_predictions[name] = model.predict(encoded_features)  # Computing predictions for each model and each record
-
+    # Model was trained on log(SalePrice)
     adjusted_predictions = {}
     
-    adjusted_predictions['ID'] = int(data_ID)
+    adjusted_predictions['Id'] = data_ID.astype(int)
     
-    for name, model in models.items():
-        # actual predictions = exp(log_predictions)
-        adjusted_predictions[name] = np.expm1(log_predictions[name])
-        if labeled:
-            #  Computing RMSE if actual data is available
-            RMSEs[name] = np.sqrt(mean_squared_error(adjusted_predictions[name], actuals))
+    # actual predictions = exp(log_predictions)
+    adjusted_predictions['predicted_sale_price'] = np.round(np.expm1(log_predictions), 2)
 
-    # Use below with schema array_doubles
-    out = np.round(np.array(pd.DataFrame(adjusted_predictions)),2).tolist() 
+    # return an array of dictionaries such as
+    # [{'Id': 1461, 'predicted_sale_price': 120429.01}, 
+    #  {'Id': 1462, 'predicted_sale_price': 123970.31}]
+    
+    print("\noutput: ", pd.DataFrame(adjusted_predictions).to_dict(orient='records'), flush=True)
 
-    # Use below with schema three_RMSEs
-    #out = RMSEs
-    yield out
+    yield pd.DataFrame(adjusted_predictions).to_dict(orient='records')
+
 
 # modelop.metrics
 def metrics(datum):
-    yield {"foo": 1}
+
+    print()
+    print(type(datum), flush=True)
+    print()
+    print(datum.shape, flush=True)
+    print()
+    
+    #datum = pd.DataFrame([np.array(datum)[0][0]])
+    datum = pd.DataFrame(datum)
+
+    adjusted_predictions = datum['predicted_sale_price']
+    actuals = datum['actual_sale_price']
+
+    # Mean squared error
+    MSE = mean_squared_error(adjusted_predictions, actuals)
+    RMSE = np.sqrt(MSE)
+    MAE = mean_absolute_error(adjusted_predictions, actuals)
+
+    yield {
+        "RMSE": np.round(RMSE, 2),
+        "MAE": np.round(MAE, 2)
+    }
